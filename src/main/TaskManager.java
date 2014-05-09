40a93997838fe5ca10eeb1213a5bab59c8223eab
fdgh <fdgh88@mail.ru>
2014-05-09 20:36:49 +0400

    Update TaskManager.java

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -10,53 +10,62 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class TaskManager {		
-	public static void main(String[] args) {
-		ExecutorService exec = Executors.newCachedThreadPool();
-		List<int[]> data = null;
-		try {
-			// if input file name is not specified , then program uses 'input.txt' by default
-			data = args.length == 0 ? getInputDataFromFile("input.txt") : getInputDataFromFile(args[0]);
+public class TaskManager {
+	final static int MAX_COUNT_OF_THREADS = 4;
+	
+	public static void main(String[] args) {	
+		try {	
+			ExecutorService exec = Executors.newFixedThreadPool(MAX_COUNT_OF_THREADS);
 			
-			if (data == null) throw new IllegalStateException();
-			List<Future<Integer>> resultList = new ArrayList<Future<Integer>>(data.size());
-			for (int[] inputMas : data) {
-				 resultList.add(exec.submit(new Worker(inputMas)));
+			String inputFileName;
+			// if input file name is not specified , then program uses 'input.txt' by default
+			inputFileName = args.length == 0 ? "input.txt" : args[0];
+			
+			final String[] inputData = getInputDataFromFile(inputFileName);	
+			
+			if (inputData == null) throw new IllegalStateException();		
+			
+			List<Future<Integer>> resultList = new ArrayList<Future<Integer>>();
+			final int sizeOfPortion = inputData.length / MAX_COUNT_OF_THREADS;
+			if (sizeOfPortion == 0) {
+				resultList.add(exec.submit(new Worker(inputData)));
+			} else {
+				for ( int i = 0; i < MAX_COUNT_OF_THREADS - 1; i++) {
+					resultList.add(exec.submit(new Worker(inputData, i * sizeOfPortion, (i + 1) * sizeOfPortion)));
+				}
+				resultList.add(exec.submit(new Worker(inputData, (MAX_COUNT_OF_THREADS - 1) * sizeOfPortion, inputData.length - 1)));
 			}
-			int result = exec.submit(new Worker(resultList)).get();
-			System.out.print("The max value is " + Integer.toString(result));
+			//Executor will not accept new threads
+			//and will finish all existing threads in the queue
+			exec.shutdown();
+			// wait until all threads are finish
+			while(!exec.isTerminated()) {				
+			}
+			int result = Integer.MIN_VALUE;	
+			for (int i = 0; i < resultList.size(); i++) {
+				result = Math.max(resultList.get(i).get(), result);
+			}
+			
+			System.out.println("The max value is " + Integer.toString(result));
+		} catch (NumberFormatException e) {
+			System.err.println("Can't parse to int");
+			System.err.println(e.getMessage());
+		} catch (FileNotFoundException e) {
+			System.err.println("Can't find input file.");
+		} catch (IOException e) {
+			System.err.println("Can't read from this file.");
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			exec.shutdown();
-		}
+		} 
 	}
 
-	private static List<int[]> getInputDataFromFile(String fileName) {
+	private static String[] getInputDataFromFile(String fileName) throws IOException {
 		if (fileName == null) throw new NullPointerException("File name can't be null");
 		BufferedReader br = null;
-		List<int[]> list = null;
+		String[] inputData = null;
 		try {
 			br = new BufferedReader(new FileReader(fileName));
-			list = new ArrayList<int[]>();
-			String[] inputData = br.readLine().split(" ");
-			//TODO to know how to find it
-			int sizeOfPortion = inputData.length;
-			if (sizeOfPortion / 4 > 0) sizeOfPortion /= 4;
-			int j = 0;
-			while (j < inputData.length) {
-				//size of tempMas equals sizeOfPotrion or less 
-				int[] tempMas = new int[Math.min(sizeOfPortion, inputData.length - j)];
-				for (int i = 0; i < sizeOfPortion && j < inputData.length; i++) {
-					tempMas[i] = Integer.parseInt(inputData[j]);
-					j++;
-				}
-				list.add(tempMas);
-			}
-		} catch (FileNotFoundException e) {
-			System.err.print("Can't find input file.");
-		} catch (IOException e) {
-			System.err.print("Can't read from this file.");
+			inputData = br.readLine().split(" ");			
 		} finally {
 			try {
 				if (br != null) br.close();
@@ -65,6 +74,6 @@ public class TaskManager {
 			}
 		}
 
-		return list;
+		return inputData;
 	}
 }
