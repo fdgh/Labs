4891ee40732fd7aa7ea0877f8cebd6264c375ade
fdgh <fdgh88@mail.ru>
2014-05-18 22:50:15 +0400

    Update Worker.java

--- src/main/Worker.java
+++ src/main/Worker.java
@@ -3,34 +3,34 @@ package main;
 import java.util.concurrent.Callable;
 
 public class Worker implements Callable<Integer>{
-	private int[] mas;
+	int max;
 
 	@Override
 	public Integer call() throws Exception {
-		return getMax();
-	}
-	
-	private int getMax() {
-		int max = Integer.MIN_VALUE;
-		for (int t : mas) {
-			max = Math.max(t, max);
-		}
 		return max;
 	}
-
-	public Worker(int[] mas) {
-		this.mas = mas;
+	
+	/**
+	 * Parse Strings and find max number between them
+	 * @param mas contains mas of numbers, can't be empty or null
+	 * 
+	 */
+	public Worker(String[] mas) throws IllegalArgumentException {
+		this(mas, 0, mas.length - 1);		
 	}
 	
-	public Worker(String[] mas) {
-		this(mas, 0, mas.length - 1);
-	}
-	
-	public Worker(String[] mas, int start, int end) {
-		this.mas = new int[end - start + 1];
-		//TODO ask about it
-		for (int i = start, j = 0 ; i <= end; i++, j++) {
-			this.mas[j] = Integer.parseInt(mas[i]);
+	/**
+	 * Parse Strings from start position of mas to end and find max number between them
+	 * @param mas contains mas of numbers, can't be empty or null
+	 * @param start position of mas from which we start to parse and find max
+	 * @param end position of mas until which we parse and find max
+	 * @throws IllegalArgumentException
+	 */	 
+	public Worker(String[] mas, int start, int end) throws IllegalArgumentException {
+		if (start > end) throw new IllegalArgumentException();
+		max = Integer.parseInt(mas[start]);
+		for (int i = start; i <= end; i++) {
+			max = Math.max(Integer.parseInt(mas[i]), max);
 		}
 	}
 	
