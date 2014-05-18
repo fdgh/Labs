2739a8409f6eca6ee5f50ff10c7a2d68f07aa699
fdgh <fdgh88@mail.ru>
2014-05-18 15:19:20 +0400

    Update TaskManager.java

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -1,44 +1,30 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class TaskManager {
-	private final static int MAX_COUNT_OF_THREADS = 4;
-	
+	private final static int MAX_COUNT_OF_THREADS_DEFAULT_VALUE = 10;
+	private final static String INPUT_FILE_NAME_DEAFAULT_VALUE = "input.txt";
+
 	public static void main(String[] args) {	
 		try {	
-			ExecutorService exec = Executors.newFixedThreadPool(MAX_COUNT_OF_THREADS);
+			// if input file name is not specified , then program uses INPUT_FILE_NAME_DEAFAULT_VALUE by default
+			 String inputFileName = args.length > 0 ?  args[0] : INPUT_FILE_NAME_DEAFAULT_VALUE;
+			 File input = new File(inputFileName);;
+			// if threads number is not specified , then program uses MAX_COUNT_OF_THREADS_DEFAULT_VALUE by default
+			int threadsNumber = args.length > 1 ? Integer.parseInt(args[1]) : MAX_COUNT_OF_THREADS_DEFAULT_VALUE; 			
 			
-			String inputFileName;
-			// if input file name is not specified , then program uses 'input.txt' by default
-			inputFileName = args.length == 0 ? "input.txt" : args[0];
-			
-			final List<String[]> inputDataList = getInputDataFromFile(inputFileName);	
-			List<Future<Integer>> resultList = new ArrayList<Future<Integer>>();
-
-			for (String[] inputData : inputDataList) {
-				if (inputData == null || inputData.length == 0) throw new IllegalStateException("Can't correctly read from input file");						
-				resultList.add(exec.submit(new Worker(inputData)));
-			}
-			//Executor will not accept new threads
-			//and will finish all existing threads in the queue
-			exec.shutdown();
-			// wait until all threads are finish
-			while(!exec.isTerminated()) {				
-			}
-			int result = Integer.MIN_VALUE;	
-			for (int i = 0; i < resultList.size(); i++) {
-				result = Math.max(resultList.get(i).get(), result);
-			}
-		
+			int result = calculateMax(threadsNumber, input);
 			System.out.println("The max value is " + Integer.toString(result));
 		} catch (NumberFormatException e) {
 			System.err.println("Can't parse to int");
@@ -51,24 +37,38 @@ public class TaskManager {
 			e.printStackTrace();
 		} 
 	}
-
-	private static List<String[]> getInputDataFromFile(String fileName) throws IOException {
-		if (fileName == null) throw new NullPointerException("File name can't be null");
+	
+	public static int calculateMax(int threadsNumber, File input) throws InterruptedException, ExecutionException, IOException {
+		ExecutorService exec = Executors.newFixedThreadPool(threadsNumber);
 		BufferedReader br = null;
-		List<String[]> inputData = new ArrayList<String[]>();
+		List<Future<Integer>> resultList = new ArrayList<Future<Integer>>();
+		
 		try {
-			br = new BufferedReader(new FileReader(fileName));
-			while(br.ready()) {
-				inputData.add(br.readLine().split(" "));
+			br = new BufferedReader(new FileReader(input));
+			while (br.ready()) {
+				String inputData = br.readLine();
+				if (inputData == null || inputData.isEmpty()) throw new IllegalStateException("Can't correctly read from input file");						
+				resultList.add(exec.submit(new Worker(inputData.split(" "))));
 			}
 		} finally {
 			try {
 				if (br != null) br.close();
 			} catch (IOException e) {
-				System.err.print("Can't close input file.");
+				e.printStackTrace();
 			}
 		}
-
-		return inputData;
+		//Executor will not accept new threads and will finish all existing threads in the queue
+		exec.shutdown();
+		// wait until all threads are finish
+		while(!exec.isTerminated()) {				
+		}
+		//calculate
+		int result = Integer.MIN_VALUE;	
+		for (int i = 0; i < resultList.size(); i++) {
+			result = Math.max(resultList.get(i).get(), result);
+		}
+		return result;	
 	}
+	
+	
 }
