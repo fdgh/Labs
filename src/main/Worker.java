21b953caa857cbdc8951cf041bd5d10d79c2de4f
fdgh <fdgh88@mail.ru>
2014-05-22 12:59:11 +0400

    Update Worker.java

--- src/main/Worker.java
+++ src/main/Worker.java
@@ -3,35 +3,22 @@ package main;
 import java.util.concurrent.Callable;
 
 public class Worker implements Callable<Integer>{
-	int max;
+	String data;
+	
 
 	@Override
 	public Integer call() throws Exception {
-		return max;
-	}
-	
-	/**
-	 * Parse Strings and find max number between them
-	 * @param mas contains mas of numbers, can't be empty or null
-	 * 
-	 */
-	public Worker(String[] mas) throws IllegalArgumentException {
-		this(mas, 0, mas.length - 1);		
-	}
-	
-	/**
-	 * Parse Strings from start position of mas to end and find max number between them
-	 * @param mas contains mas of numbers, can't be empty or null
-	 * @param start position of mas from which we start to parse and find max
-	 * @param end position of mas until which we parse and find max
-	 * @throws IllegalArgumentException
-	 */	 
-	public Worker(String[] mas, int start, int end) throws IllegalArgumentException {
-		if (start > end) throw new IllegalArgumentException();
-		max = Integer.parseInt(mas[start]);
+		String[] mas = data.split(" "); 
+		int max = Integer.parseInt(mas[start]);
 		for (int i = start; i <= end; i++) {
 			max = Math.max(Integer.parseInt(mas[i]), max);
 		}
+		return max;
+	}
+	
+	
+	public Worker(String data) throws IllegalArgumentException {
+		this.data = data;
 	}
 	
 }
