007c47d68f744cfa17179b60bc62f06e1ab91144
fdgh <fdgh88@mail.ru>
2014-05-28 20:18:12 +0400

    Update Worker.java

--- src/main/Worker.java
+++ src/main/Worker.java
@@ -2,23 +2,20 @@ package main;
 
 import java.util.concurrent.Callable;
 
-public class Worker implements Callable<Integer>{
+public class Worker implements Callable<Integer> {
 	String data;
-	
 
 	@Override
 	public Integer call() throws Exception {
-		String[] mas = data.split(" "); 
-		int max = Integer.parseInt(mas[start]);
-		for (int i = start; i <= end; i++) {
-			max = Math.max(Integer.parseInt(mas[i]), max);
+		String[] mas = data.split(" ");
+		int max = Integer.parseInt(mas[0]);
+		for (String s : mas) {
+			max = Math.max(Integer.parseInt(s), max);
 		}
 		return max;
 	}
-	
-	
+
 	public Worker(String data) throws IllegalArgumentException {
 		this.data = data;
 	}
-	
 }
